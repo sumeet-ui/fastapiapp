106aa2d7172f983e2824a234ae09c0bcd3bdbd8f
Sumeet <4al23ai057@aiet.org>
2026-06-26 12:35:56 +0530

    commonds

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+from routers import company,job
+
+app = FastAPI()
+
+app.include_router(company.router)
+app.include_router(job.router)
+
+@app.get("/")
+def read_rooot():
+    return{"Hello":"World"}
+
+
+@app.get("/about")
+def read_about():
+    return{"about": "this is about gobi"}
+
+@app.get("/contact")
+def read_contact():
+    return{"contact": "this contact from gobi+"}
