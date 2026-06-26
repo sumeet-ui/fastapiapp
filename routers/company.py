d7e927c8066fd67ad49e4aff2cffc38a8f0409ec
Sumeet <4al23ai057@aiet.org>
2026-06-26 16:51:04 +0530

    commonds

--- routers/company.py
+++ routers/company.py
@@ -1,11 +1,28 @@
 from fastapi import APIRouter
+from schemas.company import CompanyCreate,CompanyUpdate
 
 router = APIRouter(prefix="/company",tags=["company"])
+companies = []
+
+@router.post("/")
+def create_company(company: CompanyCreate):
+    companies.append(company)
+    return companies
 
 @router.get("/")
-def read_company():
-    return{"company": "Company root"}
+def get_all_company():
+    return companies
 
 @router.get("/{company_id}")
-def read_company(company_id: int):
-    return {"company_id" : company_id}
+def get_company(company_id: int):
+    return companies [company_id]
+
+@router.put("/{company_id}")
+def update_company(company_id: int,company: CompanyUpdate):
+    companies[company_id] = company
+    return companies 
+
+@router.delete("/{company_id}")
+def delete_company(company_id: int,company: CompanyUpdate):
+    companies[company_id] = company
+    return companies 
