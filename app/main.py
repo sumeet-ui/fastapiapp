22a215d7230a4ce5848eb67c7f6ed2634395b979
Sumeet <4al23ai057@aiet.org>
2026-06-27 14:15:17 +0530

    commonds

--- app/main.py
+++ app/main.py
@@ -1,20 +1,28 @@
 from fastapi import FastAPI
-from routers import company,job
+from routers import company, job
+from database import Base, engine
+from models.company import Company
+from models.job import Job
 
-app = FastAPI()
+app=FastAPI()
+print("engine is :",engine)
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(company.router)
 app.include_router(job.router)
 
 @app.get("/")
-def read_rooot():
-    return{"Hello":"World"}
+def read_root():
+    return{ "Hello"  : "World"}
 
 
 @app.get("/about")
 def read_about():
-    return{"about": "this is about gobi"}
+    return {"About": "This is about page."}
+
+
 
 @app.get("/contact")
 def read_contact():
-    return{"contact": "this contact from gobi+"}
+    return {"Contact": "This is contact page."}
--- schemas/job.py
+++ schemas/job.py
@@ -1,10 +1,24 @@
-from  pydantic import BaseModel
-from  typing import Optional
+from pydantic import BaseModel
+from typing import Optional
 
-class JobCreate(BaseModel):
-    title : str
-    salary : int
+class JobBase(BaseModel):
+    title: str
+    salary: int
+    description: Optional[str] = None
+    company_id: int
 
-class JobUpdate(BaseModel):
-    title : Optional[str] = None
-    salary : Optional[int] = None
+class JobCreate(JobBase):
+    pass
+
+class JobUpdate(JobBase):
+    title: Optional[str] = None
+    salary: Optional[int] = None
+    description: Optional[str] = None
+    company_id: Optional[int] = None
+
+class JobResponse(JobBase):
+    id: int
+    company_id: int
+
+    class Config:
+        from_attributes = True
