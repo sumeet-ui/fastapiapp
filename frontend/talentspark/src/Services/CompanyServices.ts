ba502da73928bcf5e25293246263fa5ec36d8f45
Sumeet <4al23ai057@aiet.org>
2026-06-30 14:23:28 +0530

    .

--- frontend/talentspark/src/Services/CompanyServices.ts
+++ frontend/talentspark/src/Services/CompanyServices.ts
@@ -4,6 +4,6 @@ import type {Company} from "../types/company";
 const API_BASE_URL ="https://localhost:8000";
 
 export async function getCompanies(): Promise<Company[]> { 
-    const response = await axios.get('${API_BASE_URL}/companies');
+    const response = await axios.get(`${API_BASE_URL}/companies`);
     return response.data;
 }
